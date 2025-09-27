d3e0dbb71b1a74cbf59c4ebe12f3ca8d7bf41561
ALNoman78 <abdullaalno078@gmail.com>
2025-09-27 22:07:53 +0600

    add done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,14 +8,18 @@ const Hero = () => {
     return (
         <div id='hero' className="hero bg-base-200 min-h-screen">
             <div className="hero-content md:w-[1280px] mx-auto flex-col justify-between lg:flex-row-reverse">
-                <motion.img
+                <motion.div
+                className='md:w-2xl'
                     initial={{ opacity: 0, x: 40 }}
                     whileInView={{ opacity: 1, x: 0 }}
                     transition={{ duration: 0.6 }}
                     viewport={{ once: true }}
-                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                    className="max-w-sm rounded-lg shadow-2xl"
-                />
+                >
+                    <img
+                        src="https://i.ibb.co.com/SDgCXwsS/pp2.jpg"
+                        className="w-full h-[800px] object-cover rounded-lg shadow-2xl"
+                    />
+                </motion.div>
                 <motion.div
                     initial={{ opacity: 0, x: -40 }}
                     whileInView={{ opacity: 1, x: 0 }}
@@ -23,7 +27,7 @@ const Hero = () => {
                     viewport={{ once: true }}
                     className='md:w-2xl'
                 >
-                    <h1 className="text-5xl font-bold">3M</h1>
+                    <h1 className="text-5xl font-bold">Maisa Monoara Moule</h1>
                     <p className="py-6 md:w-[600px]">
                         A passionate and results-driven Digital Marketing Expert who helps businesses grow their online presence with smart, data-driven strategies. With expertise in SEO, social media management, content marketing, and paid advertising, impactful campaigns are crafted to reach the right audience and deliver measurable results.
                     </p>
